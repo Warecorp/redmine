6a20d5c6e7b70d662ea6bc5bc6ca76dd0ecce8a2
Boris Efron <borisdr@gmail.com>
2015-09-30 09:14:18 +0300

    qpharmetra branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :rails_env, 'staging'
 set :branch,    fetch(:branch, 'develop')
 
-server 'trainer.warecorp.com', user: 'redmine', roles: [:app, :db, :util, :web, :assets]
+server 'qpharmetra-stage.warecorp.com', user: 'redmine', roles: [:app, :db, :util, :web, :assets]
